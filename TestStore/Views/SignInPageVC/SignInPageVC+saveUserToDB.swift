3c20761dbfdeada2f0b35fdfce84e9f301a2ec2c
Alexander_Borisov <borisovab@mail.ru>
2023-03-19 18:16:37 +0300

    AddedValidationEmail
    Added checking the spelling of the email address (validation)

--- TestStore/Views/SignInPageVC/SignInPageVC+saveUserToDB.swift
+++ TestStore/Views/SignInPageVC/SignInPageVC+saveUserToDB.swift
@@ -10,52 +10,68 @@ import CoreData
 
 extension SignInPageViewController {
     func saveUserData() -> Bool {
-        if firstNameTF.text!.isEmpty || lastNameTF.text!.isEmpty || emailTF.text!.isEmpty {
-            errorAlert()
-            return false
-        }
-
-        // создаем объект
-        var userData = viewModel?.user
-
-        if userData == nil {
-            userData = UserData()
-        }
-
-        // сохраняем объект
-        if let userData = userData {
-
-            // достаем из БД данные
-            let fetchResult = NSFetchRequest<NSFetchRequestResult>(entityName: "UserData")
-
-            // проверка, если введенные данные уже имеются в БД, выдать ошибку, если нет, записать в БД
-            do {
-                let results = try CoreDataManager.instance.context.fetch(fetchResult)
-                var flag = false
-                for result in results as! [UserData] {
-                    let email = result.email
-
-                    if emailTF.text == email {
-                        flag = false
-                        break
-                    } else {
-                        flag = true
-                    }
-                }
-
-                if flag == false {
-                    errorNameOrEmailAlert()
+        var flag = false
+        if let firstName = firstNameTF.text, let lastName = lastNameTF.text, let email = emailTF.text {
+            if firstName.isEmpty || lastName.isEmpty || email.isEmpty {
+                UtilityFunction().simpleAlert(vc: self, title: "Ошибка", message: "Пожалуйста, заполните все поля для регистрации личного кабинета")
+                flag = false
+            } else {
+                if !email.isValidEmail(email: email) {
+                    UtilityFunction().simpleAlert(vc: self, title: "Ошибка", message: "Пожалуйста, укажите верный email.")
+                    flag = false
                 } else {
-                    userData.firstName = firstNameTF.text
-                    userData.lastName = lastNameTF.text
-                    userData.email = emailTF.text
-                    CoreDataManager.instance.saveContext()
-                }
+                    // создаем объект
+                    var userData = viewModel?.user
 
-            } catch let error as NSError {
-                print(error.localizedDescription)
+                    if userData == nil {
+                        userData = UserData()
+                    }
+
+                    // сохраняем объект
+                    if let userData = userData {
+
+                        // достаем из БД данные
+                        let fetchResult = NSFetchRequest<NSFetchRequestResult>(entityName: "UserData")
+
+                        // проверка, если введенные данные уже имеются в БД, выдать ошибку, если нет, записать в БД
+                        do {
+                            let results = try CoreDataManager.instance.context.fetch(fetchResult)
+                            var flag = false
+                            for result in results as! [UserData] {
+                                let email = result.email
+
+                                if emailTF.text == email {
+                                    flag = false
+                                    break
+                                } else {
+                                    flag = true
+                                }
+                            }
+
+                            if flag == false {
+                                UtilityFunction().simpleAlert(vc: self, title: "Ошибка", message: "Указанный email уже зарегистрирован ранее.")
+                            } else {
+                                userData.firstName = firstNameTF.text
+                                userData.lastName = lastNameTF.text
+                                userData.email = emailTF.text
+                                CoreDataManager.instance.saveContext()
+                            }
+
+                        } catch let error as NSError {
+                            print(error.localizedDescription)
+                        }
+                    }
+                    flag = true
+                }
             }
         }
-        return true
+
+
+
+
+
+
+//        return true
+        return flag
     }
 }
--- TestStore/Views/SignInPageVC/SignInPageViewController.swift
+++ TestStore/Views/SignInPageVC/SignInPageViewController.swift
@@ -174,3 +174,5 @@ class SignInPageViewController: UIViewController, AdapterViewController {
 
 }
 
+
+
